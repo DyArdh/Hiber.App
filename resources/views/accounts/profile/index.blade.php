40a8b37deba369541e762ef49c70e347b1bd83fe
DyArdh <rizuna30@gmail.com>
2022-04-15 20:57:08 +0700

    penyortiran

--- resources/views/accounts/profile/index.blade.php
+++ resources/views/accounts/profile/index.blade.php
@@ -27,19 +27,6 @@
         </div>
 
         <div class="row mt-4 mx-3  fw-bold">
-            <div class="col-3 col-sm-3">Password</div>
-            <div class="col-9 col-sm-2">:</div>
-            <div class="col-12 col-sm-7 text-start text-sm-end">
-                <a href="#">
-                    <button type="button" class="btn btn-primary" id="tambah-acc">
-                      <i class="fa-solid fa-lock"></i>
-                      <span class="ps-2 fw-bolder"> Password</span>
-                    </button>
-                </a>   
-            </div>
-        </div>
-
-        <div class="row mt-4 mx-3  fw-bold">
             <div class="col-3 col-sm-3">No. HP</div>
             <div class="col-9 col-sm-2">:</div>
             <div class="col-12 col-sm-7 text-start text-sm-end">{{ $user->no_hp }}</div>
@@ -49,20 +36,6 @@
             <div class="col-3 col-sm-3">Role</div>
             <div class="col-9 col-sm-2">:</div>
             <div class="col-12 col-sm-7 text-start text-sm-end">{{ $user->role }}</div>
-        </div>
-
-        <div class="row mt-3 mb-4 mx-3  fw-bold">
-            <hr style="height: 5px">
-            <div class="col-3 col-sm-3"></div>
-            <div class="col-9 col-sm-2"></div>
-            <div class="col-12 col-sm-7 text-end">
-                <a href="#">
-                    <button type="button" class="btn btn-primary" id="tambah-acc">
-                      <i class="fa-solid fa-pencil"></i>
-                      <span class="ps-2 fw-bolder"> Edit Profile</span>
-                    </button>
-                </a>  
-            </div>
         </div>
     </div>
 </div>
