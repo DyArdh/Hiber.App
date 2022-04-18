9fe124fb6058d24c0ef079f841f8e15676171823
DyArdh <rizuna30@gmail.com>
2022-04-18 11:07:24 +0700

    update top navbar

--- resources/views/layouts/navbar/top-navbar.blade.php
+++ resources/views/layouts/navbar/top-navbar.blade.php
@@ -8,7 +8,11 @@
         <!-- Navbar-->
         <ul class="navbar-nav ms-auto ms-md-0 me-3 me-lg-4">
           <li class="nav-item dropdown">
-            <a class="nav-link dropdown-toggle" id="navbarDropdown" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false"><i class="fas fa-user fa-fw" style="color: #434a6a"></i></a>
+            <a class="nav-link dropdown-toggle" id="navbarDropdown" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+              <i class="fas fa-user fa-fw" style="color: #434a6a"></i>
+              <span class="user-role" style="color: #434a6a">{{ Auth::User()->role }}</span>
+              <i class="fas fa-angle-down fa-fw" style="color: #434a6a"></i>
+            </a>
             <ul class="dropdown-menu dropdown-menu-end" aria-labelledby="navbarDropdown">
               <li><a class="dropdown-item" href="{{ route('profile') }}">Account</a></li>
               <li><hr class="dropdown-divider" /></li>
