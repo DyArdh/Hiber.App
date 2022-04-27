5811bfedc3cb19cca45096a2aad372e227198ecf
DyArdh <rizuna30@gmail.com>
2022-04-27 16:50:28 +0700

    edit header

--- resources/views/layouts/navbar/sidebar.blade.php
+++ resources/views/layouts/navbar/sidebar.blade.php
@@ -66,7 +66,7 @@
             </nav>
           </div>
 
-          <div class="sb-sidenav-menu-heading">Interface</div>
+          <div class="sb-sidenav-menu-heading">Sale</div>
             <a class="nav-link collapsed" href="#" data-bs-toggle="collapse" data-bs-target="#collapsePenjualan" aria-expanded="false" aria-controls="collapseLayouts">
             <div class="sb-nav-link-icon"><i class="fas fa-cart-shopping"></i></div>
             Penjualan
